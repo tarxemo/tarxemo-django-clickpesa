0d98c0b6de3282a35db037cb12ee51989a6a6ed2
tarxemo <tarxemo@gmail.com>
2026-01-12 11:01:21 +0300

    modified the configurations to support the git workflow

--- clickpesa/__init__.py
+++ clickpesa/__init__.py
@@ -4,6 +4,6 @@ ClickPesa Payment Utility for Django
 A modular, reusable utility for integrating ClickPesa mobile money payments and payouts.
 """
 
-__version__ = "1.0.0"
+__version__ = "0.1.1"
 
 default_app_config = 'clickpesa.apps.BhumwiPaymentsConfig'
