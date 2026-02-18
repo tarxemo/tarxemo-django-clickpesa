d5bfcae8dcd21e3ef2eb5e9dfbfe3f218297183e
tarxemo <tarxemo@gmail.com>
2026-02-18 03:10:48 +0300

    set ups implemented

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tarxemo-django-clickpesa",
-    version="0.2.4",
+    version="0.2.5",
     author="TarXemo",
     description="A Django library for integrating ClickPesa mobile money and payout services",
     long_description=long_description,
